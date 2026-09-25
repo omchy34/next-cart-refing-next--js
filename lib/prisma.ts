0a2cd86b3476de22ae9b8e9e7c45033f388d909d
omchy34 <omchy34@gmail.com>
2026-09-25 15:10:03 +0530

    bug fixed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
-import 'dotenv/config'
-import { PrismaClient } from './generated/prisma'
+import 'dotenv/config';
+import { PrismaClient } from "../generated/prisma/client" ;
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 const adapter = new PrismaNeon({
